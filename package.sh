b3a71cf302c5c9a60c5ecc7d9495e93c0c34beff
ProgrammerIn-wonderland <3838shah@gmail.com>
2025-04-08 14:06:22 -0400

    install local npm packages in package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd myDav && npm i && npm run build && cd ..
+npm i && cd myDav && npm i && npm run build && cd ..
 
 zip vscode.app.zip $( find -not -path "./http.js" -not -path "./package.sh" -not -path "vscode.app.zip" -not -path "./node_modules/*" -not -path "./myDav/node_modules/*" -not -path "./.git/*" && find "./node_modules/vscode-web" )
